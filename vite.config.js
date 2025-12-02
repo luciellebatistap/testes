8f89dbf37dc6114356aad1a03d5275fa42daac43
luciellebatistap <luciellebatistap@gmail.com>
2025-12-02 15:46:28 -0300

    5

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  // O 'base' é o nome do seu repositório no GitHub
-  base: '/testes/',  
   plugins: [react()],
 })
